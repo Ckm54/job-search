a18353407307bc2c9cfc03cbb3fba5ba5dc7ecee
collo <mutaic84@gmail.com>
2023-02-02 11:32:54 +0300

    :technologist: :hammer: provide context to application

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createTheme, ThemeProvider } from "@mui/material";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import JobsProvider from "./context/jobsContext";
 import "./index.css";
 
 const theme = createTheme({
@@ -16,8 +17,10 @@ const theme = createTheme({
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <JobsProvider>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </JobsProvider>
   </React.StrictMode>
 );
