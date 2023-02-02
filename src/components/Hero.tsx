808fc75d94d5e0acdc532828bcab20ece73efc9f
collo <mutaic84@gmail.com>
2023-02-02 09:03:07 +0300

    :lipstick: :hammer: update hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,15 @@
+import { Container, Stack, Typography } from '@mui/material';
 import React from 'react';
+import { palleteColor } from '../themes/palleteColor';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <Container>
+      <Stack width={{lg: 3/4}}>
+        <Typography fontSize={{lg: '64px', xs: '30px'}} fontWeight={'bold'} >Find a job that best fits your lifestyle.</Typography>
+        <Typography color={`${palleteColor.grayPallete}`} fontSize={{lg: '24px'}} >Discover the best opportunities around you</Typography>
+      </Stack>
+    </Container>
   )
 }
 
